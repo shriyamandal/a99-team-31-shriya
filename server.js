36d39076f1d9085296c81369d96a713e50e23424
Shriya Mandal <69815807+shriyamandal@users.noreply.github.com>
2022-11-27 20:16:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-e
+#!/usr/bin/env node
+
+import { getTeachers, computeDifficulty, computeRating } from './lib/rmp.js';
+import express from "express";
+import minimist from "minimist";
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+const args = minimist(process.argv.slice(2));
+const port = args.port || 5000;
+
+app.get('/app', (req, res) => {
+    res.status(200).send('200 OK');
+  })
+
+app.get('/app/ratings/', async(req, res) => {
+    let difficulty = await computeDifficulty;
+    res.send(difficulty);
+})
+
+app.get("*",(req, res) => {
+	res.status(404).send("404 NOT FOUND");
+})
+
+app.listen(port);
