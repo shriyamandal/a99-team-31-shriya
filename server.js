c0ef7c014cd23ec97e8104df38fd14da89dd2229
Shriya Mandal <69815807+shriyamandal@users.noreply.github.com>
2022-12-03 00:17:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.get('/app/ratings/:teachers/', async(req, res) => {
 
 app.get('/app/difficulty/:teachers/', async(req, res) => {
     console.log("got here")
-    console.log((req.body));
-    const teachersArr = (req.body.teachers).split(',')
+    console.log((req.params.teachers));
+    const teachersArr = (req.params.teachers).split(',')
     let difficulty = await computeDifficulty(teachersArr);
     res.send(difficulty);
   })
